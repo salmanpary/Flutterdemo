dc4a18b67991ffda85a7c49f850844c0ef04a86f
salmanpary <salmanpary@gmail.com>
2021-08-11 22:40:14 +0530

    code reformatted

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,20 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-     home:Scaffold(
-       body:Center(
-         child: Image(
-           image: AssetImage('images/bec.jpg'
-
-           ),
-         ),
-       ),
-       appBar: AppBar(
-         backgroundColor: Colors.red,
-         title: Center(child: Text("kannapi rocks"
-         ),
-         ),
-       ),
-     backgroundColor: Colors.black,
-     ),
+      home: Scaffold(
+        body: Center(
+          child: Image(
+            image: AssetImage('images/bec.jpg'),
+          ),
+        ),
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Center(
+            child: Text("kannapi rocks"),
+          ),
+        ),
+        backgroundColor: Colors.black,
+      ),
     ),
   );
 }

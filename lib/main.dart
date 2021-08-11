c33a539755556c35b3ebac722518264e028a001b
salmanpary <salmanpary@gmail.com>
2021-08-11 22:25:15 +0530

    Demo project finished

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+//starting point of all flutter apps
+void main() {
+  runApp(
+    MaterialApp(
+     home:Scaffold(
+       body:Center(
+         child: Image(
+           image: AssetImage('images/bec.jpg'
+
+           ),
+         ),
+       ),
+       appBar: AppBar(
+         backgroundColor: Colors.red,
+         title: Center(child: Text("kannapi rocks"
+         ),
+         ),
+       ),
+     backgroundColor: Colors.black,
+     ),
+    ),
+  );
+}
